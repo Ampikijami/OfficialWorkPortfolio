8e0c4b9a74c568d4800a61860c02e226212f8d0a
ruleram <rulecopper@hotmail.com>
2023-10-04 02:18:59 +0200

    Adding Swapping SqliteDatabaseWithJsonDatabase demonstration. sqliteDatabase.cs is an alternative implementation of an sqlite database class but sqliteDatabase2.cs is sufficient.

---
+++ SwappingSqliteDatabaseWithJsonDatabase/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace SwappingSqliteDatabaseWithJsonDatabase
+{
+    /*
+     Program summary: This program demonstrates how to program against an interface,
+    which makes swapping between a .json type database and a sqlite type database effortless.
+     */
+    class Program
+    {
+        //In order to allow child methods of the Main() thread to run asynchronously, I made static Main(string[] args) into >> static async Task Main(string[] args)
+        static async Task Main(string[] args)
+        {
+            Assembly DLL = Assembly.LoadFile(Path.Combine(Directory.GetCurrentDirectory() + "\\" + "SwappingSqliteDatabaseWithJsonDatabase.dll"));
+            Type sqliteDatabase = DLL.GetType("SwappingSqliteDatabaseWithJsonDatabase.SqliteDatabase"); //this would be helpful if the type that we wanted to use was defined in another assembly.
+            Type jsonDatabase = DLL.GetType("SwappingSqliteDatabaseWithJsonDatabase.JsonDatabase"); //this would be helpful if the type we wanted to use was defined in another assembly.
+
+            object database;
+            char userInput;
+            do
+            {
+                System.Console.WriteLine();
+                System.Console.WriteLine("Choose a database type to run...");
+                System.Console.WriteLine("1. SQLiteDatabase");
+                System.Console.WriteLine("2. JsonDatabase");
+                System.Console.WriteLine("3. JsonDatabase version 2 (Coming soon!)");
+                System.Console.WriteLine();
+                userInput = System.Console.ReadKey().KeyChar;
+
+            } while (userInput != '1' && userInput != '2'); // && userInput != '3'
+            switch (userInput)
+            {
+                case '1':
+                    database = database = Activator.CreateInstance(typeof(SwappingSqliteDatabaseWithJsonDatabase.SqliteDatabase2.SqliteDatabase2));
+                    break;
+                case '2':
+                    database = Activator.CreateInstance(typeof(JsonDatabase));
+                    break;
+                case '3':
+                    database = Activator.CreateInstance(typeof(SwappingSqliteDatabaseWithJsonDatabase.SqliteDatabase.SqliteDatabase));
+                    break;
+                default:
+                    throw new Exception("This should be impossible!");
+            };
+            MethodInfo ConnectMethod = database.GetType().GetMethod("Connect");
+            MethodInfo CreateTableMethod = database.GetType().GetMethod("CreateTable");
+            MethodInfo AddToTableMethod = database.GetType().GetMethod("AddToTable");
+            MethodInfo PrintContentsOfTableMethod = database.GetType().GetMethod("PrintContentsOfTable");
+            MethodInfo SaveMethod = database.GetType().GetMethod("Save");
+
+            ConnectMethod.Invoke(database, null);
+            CreateTableMethod.Invoke(database, new object[] { typeof(Movie), null });//create a Movie table
+            AddToTableMethod.Invoke(database, new object[] { typeof(Movie), new Movie(1, "Men in Black", 2000) });
+            AddToTableMethod.Invoke(database, new object[] { typeof(Movie), new Movie(2, "Shrek", 2005) });
+            PrintContentsOfTableMethod.Invoke(database, new object[] { typeof(Movie)});
+            SaveMethod.Invoke(database, null);
+        }
+    }
+}
---
+++ SwappingSqliteDatabaseWithJsonDatabase/sqliteDatabase.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Threading;
+using SwappingSqliteDatabaseWithJsonDatabase.SqliteDatabase;
+using Microsoft.Data.Sqlite;
+
+namespace SwappingSqliteDatabaseWithJsonDatabase.SqliteDatabase
+{
+    class SqliteDatabase : IDatabase
+    {
+        private const string dbName = "sqliteproductiondb.db";
+        private sqlitedbContext dbContext;
+        
+        public async void Connect()
+        {
+            System.Console.WriteLine("Connecting...");
+            if (File.Exists(dbName))
+                File.Delete(dbName);
+
+            dbContext = new sqlitedbContext();
+            await dbContext.Database.EnsureCreatedAsync();
+        }
+        public void CreateTable(Type tableDataType, object schema)
+        {
+            //attach/ entry
+            //dbContext.Entry()
+           // dbContext.Attach(schema);
+            //dbContext.SaveChanges();
+        }
+        public async void AddToTable(Type tableDataType, object objectInstance)
+        {
+            try
+            {
+                dbContext.Entry(
+                    new Movie(){id = 1, title = "Men In Black", releaseYear = 2001}
+                );
+                //dbContext.Attach(new Movie[]
+                //{
+                //    new Movie(){id = 1, title = "Men In Black", releaseYear = 2001},
+                //    new Movie(){id = 2,  title = "Shrek", releaseYear = 2005 }
+                //});
+                //await dbContext.Movies.AddRangeAsync(entities: new Movie[]
+                //    {
+                //        new Movie(){id = 1, title = "Men In Black", releaseYear = 2001},
+                //        new Movie(){id = 2,  title = "Shrek", releaseYear = 2005 }
+                //    });
+                await dbContext.SaveChangesAsync();
+            }
+            catch (Microsoft.EntityFrameworkCore.DbUpdateException e)
+            {
+                System.Console.WriteLine(e.Message);
+                System.Console.WriteLine(e.InnerException);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+
+        }
+        public void PrintContentsOfTable(Type tableDataType)
+        {
+            Console.WriteLine("getting database data");
+            dbContext.Movies?.ToList().ForEach(movie =>
+            {
+                System.Console.WriteLine($"{movie.title} was released in {movie.releaseYear}");
+            });
+
+            System.Console.ReadLine();
+        }
+        public bool Save()
+        {
+            try
+            {
+                dbContext.SaveChangesAsync();
+                return true; //true = success
+            }
+            catch(Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+                return false; //false = fail
+            }
+        }
+    }
+    internal class sqlitedbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("FileName=sqliteproductiondb.db", sqliteOptionsAction => {
+                sqliteOptionsAction.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().ToTable("Movies", schema: "test");
+            modelBuilder.Entity<Movie>(entity =>
+            {
+                entity.HasKey(movie => movie.id);
+                entity.HasIndex(index => index.title).IsUnique();
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
